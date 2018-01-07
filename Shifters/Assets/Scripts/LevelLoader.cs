e8e48a8b656a3bf8fc657b0490c6a26db92127c6
Joecool48 <30698327+Joecool48@users.noreply.github.com>
2018-01-06 20:29:43 -0600

    Reconfigured the file layout, fixed more bugs, and made the level selection screen from scratch

---
+++ Shifters/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour {
+	private Button button;
+	private Text text;
+	// Use this for initialization
+	void Start () {
+		button = (Button) transform.Find ("Button").GetComponent<Button> ();
+		text = (Text)transform.Find ("Button/Text").GetComponent<Text> ();;
+		button.onClick.AddListener(delegate { OnClick (name); });
+	}
+	void OnClick (string name) {
+		SceneManager.LoadScene (name);
+	}
+	// Update is called once per frame
+	void Update () {
+	}
+}
